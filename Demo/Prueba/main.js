c25ba4d76110f9895bb518864272be201682f5f7
JGM10 <javier10gm@gmail.com>
2024-05-26 11:57:09 +0200

    cookies

--- Demo/Prueba/main.js
+++ Demo/Prueba/main.js
@@ -1,4 +1,4 @@
-document.querySelector("#contador").innerHTML = Number(localStorage.getItem("contador")) || 0;
+document.querySelector("#contador").innerHTML = Number(getCookie("contador")) || 0;
 
 const boton = document.querySelector("#boton");
 boton.addEventListener(
@@ -8,9 +8,28 @@ boton.addEventListener(
 );
 
 function incremento(){
-    const contador = Number(localStorage.getItem("contador")) || 0;
+    const contador = Number(getCookie("contador")) || 0;
     document.querySelector("#contador").innerHTML = contador + 1;
-    localStorage.setItem("contador",contador + 1);
+    setCookie("contador", contador + 1, 7);
 }
 
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
